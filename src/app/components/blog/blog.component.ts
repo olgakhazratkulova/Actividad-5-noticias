bfb16ca47b39be7b2ef455612e756d17ddaa84f1
olgakh <ola_la_84@yahoo.com>
2023-02-13 15:22:37 +0100

    - Estilos aplicados
    - cambios de la estructura html

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -25,7 +25,7 @@ export class BlogComponent implements OnInit{
       {
         title: 'Rosas',
         img: 'https://www.verdissimo.com/wp-content/uploads/2018/05/tipos-de-rosas-verdissimo.jpg',
-        text: 'El género Rosa está compuesto por un conocido grupo de arbustos generalmente espinosos y floridos representantes principales de la familia de las rosáceas. Se denomina rosa a la flor de los miembros de este género y rosal a la planta. El número de especies ronda las cien, la mayoría originarias de Asia y un reducido número nativas de Europa, Norteamérica y África noroccidental. Tanto especies como cultivares e híbridos se cultivan como ornamentales por la belleza y fragancia de su flor; pero también para la extracción de aceite esencial, utilizado en perfumería y cosmética, usos medicinales (fitoterapia) y gastronómicos. Existe una enorme variedad de cultivares de rosa (más de 30 000) a partir de diversas hibridaciones, y cada año aparecen otros nuevos. Las especies progenitoras mayormente implicadas en los cultivares son: Rosa moschata, Rosa gallica, Rosa × damascena, Rosa wichuraiana, Rosa californica y Rosa rugosa. Los cultivadores de rosas o rosalistas del siglo xx se centraron en el tamaño y el color, para producir flores grandes y atractivas, aunque con poco o ningún aroma. Muchas rosas silvestres y «pasadas de moda», por el contrario, tienen una fragancia dulce y fuerte. Las flores de las las rosas están entre las flores más comunes vendidas por los floristas.',
+        text: 'El género Rosa está compuesto por un conocido grupo de arbustos generalmente espinosos y floridos representantes principales de la familia de las rosáceas. Se denomina rosa a la flor de los miembros de este género y rosal a la planta. El número de especies ronda las cien, la mayoría originarias de Asia y un reducido número nativas de Europa, Norteamérica y África noroccidental. Tanto especies como cultivares e híbridos se cultivan como ornamentales por la belleza y fragancia de su flor; pero también para la extracción de aceite esencial, utilizado en perfumería y cosmética, usos medicinales (fitoterapia) y gastronómicos. Existe una enorme variedad de cultivares de rosa (más de 30 000) a partir de diversas hibridaciones, y cada año aparecen otros nuevos. Las especies progenitoras mayormente implicadas en los cultivares son: Rosa moschata, Rosa gallica, Rosa damascena, Rosa wichuraiana, Rosa californica y Rosa rugosa. Los cultivadores de rosas o rosalistas del siglo xx se centraron en el tamaño y el color, para producir flores grandes y atractivas, aunque con poco o ningún aroma. Muchas rosas silvestres y «pasadas de moda», por el contrario, tienen una fragancia dulce y fuerte. Las flores de las las rosas están entre las flores más comunes vendidas por los floristas.',
         date: '15-07-2020',
       },
     ];
@@ -39,19 +39,27 @@ export class BlogComponent implements OnInit{
       date: this.date,
     };
 
-    this.arrNoticias.push(noticia);
+    if(this.title !== "" && this.img !== "" && this.text !== "" && this.date !== "") {
+      this.arrNoticias.push(noticia);
+
     this.cargarNoticias();
 
     this.title = '';
     this.img = '';
     this.text = '';
     this.date = '';
+    }
+    else {
+      alert("Todos los campos son obligatorios!");
+    }
+
+    
   }
 
   cargarNoticias(): void {
     this.listaNoticias = '';
     this.arrNoticias.forEach((noticia) => {
-      this.listaNoticias += `<div> <p>${noticia.title}</p> <img src = "${noticia.img}"/> <p>${noticia.text}</p> <p>${noticia.date}</p> </div>`;
+      this.listaNoticias += `<div class="card"> <h3 class="card-title">${noticia.title}</h3> <img class="card-img" src = "${noticia.img}"/> <p class="card-text">${noticia.text}</p> <p class="card-date">${noticia.date}</p> </div>`;
     });
   }
 
